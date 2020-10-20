ebbac9815e2c67f7b31042ad27f1abd3b0bd1fab
sunseekers <zhangmin.a@yuewen.com>
2020-10-20 17:18:29 +0800

    摄像头切换1

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 'use strict';
 
 const videoElement = document.querySelector('video');
-const audioInputSelect = document.querySelector('select#audioSource');
-const audioOutputSelect = document.querySelector('select#audioOutput');
 const videoSelect = document.querySelector('select#videoSource');
 const selectors = [videoSelect];
 
 let video = document.querySelector('video');
 let canvas = document.getElementById('canvas');
 let context1 = canvas.getContext('2d');
-let photograph = document.querySelector("#photograph")
 
-// audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
 
 function gotDevices(deviceInfos) {
-  // Handles being called several times to update labels. Preserve values.
   const values = selectors.map(select => select.value);
   selectors.forEach(select => {
     while (select.firstChild) {
@@ -26,7 +21,6 @@ function gotDevices(deviceInfos) {
     const deviceInfo = deviceInfos[i];
     const option = document.createElement('option');
     option.value = deviceInfo.deviceId;
-    console.log('[deviceInfo.label]', deviceInfo.label);
 
     if (deviceInfo.kind === 'videoinput') {
       option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
@@ -44,36 +38,12 @@ function gotDevices(deviceInfos) {
 
 navigator.mediaDevices.enumerateDevices().then(gotDevices)
 
-// Attach audio output device to video element using device/sink ID.
-function attachSinkId(element, sinkId) {
-  if (typeof element.sinkId !== 'undefined') {
-    element.setSinkId(sinkId)
-      .then(() => {
-        console.log(`Success, audio output device attached: ${sinkId}`);
-      })
-      .catch(error => {
-        let errorMessage = error;
-        if (error.name === 'SecurityError') {
-          errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
-        }
-        console.error(errorMessage);
-        // Jump back to first output device in the list as it's the default.
-        audioOutputSelect.selectedIndex = 0;
-      });
-  } else {
-    console.warn('Browser does not support output device selection.');
-  }
-}
 
-function changeAudioDestination() {
-  const audioDestination = audioOutputSelect.value;
-  attachSinkId(videoElement, audioDestination);
-}
+
 
 function gotStream(stream) {
   window.stream = stream; // make stream available to console
   videoElement.srcObject = stream;
-  // Refresh button list in case labels have become available
   return navigator.mediaDevices.enumerateDevices();
 }
 
@@ -87,14 +57,8 @@ function start() {
       track.stop();
     });
   }
-  // const audioSource = audioInputSelect.value;
   const videoSource = videoSelect.value;
   const constraints = {
-    // audio: {
-    //   deviceId: audioSource ? {
-    //     exact: audioSource
-    //   } : undefined
-    // },
     video: {
       deviceId: videoSource ? {
         exact: videoSource
@@ -104,8 +68,6 @@ function start() {
   navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
 }
 
-// audioInputSelect.onchange = start;
-// audioOutputSelect.onchange = changeAudioDestination;
 
 videoSelect.onchange = start;
 
